533724e188b6c3ab5b8cceb2915b29695d085e12
ahmadkhursheed <512ahmadkhursheed@gmail.com>
2021-10-24 18:39:19 +0500

    added some changes and change style

--- app/src/main/java/com/example/countapp/MainActivity.java
+++ app/src/main/java/com/example/countapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private Button increaseButton;
-    private Button decrementButton;
+    private Button decreasetButton;
     private Button Reset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        decrementButton.setOnClickListener(new View.OnClickListener() {
+        decreasetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (Integer.parseInt(textView.getText().toString() )!=0)
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     {
         textView=findViewById(R.id.TextView);
         increaseButton=findViewById(R.id.increase);
-        decrementButton=findViewById(R.id.decrease);
+        decreasetButton=findViewById(R.id.decrease);
         Reset=findViewById(R.id.reset);
     }
 
