b3e870c83454944c475c7b47a8f43e447cf39152
ahmadkhursheed <512ahmadkhursheed@gmail.com>
2021-10-26 19:19:48 +0500

    finish

--- app/src/main/java/com/example/countapp/MainActivity.java
+++ app/src/main/java/com/example/countapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private Button incrementButton;
-    private Button decreaseButton;
+    private Button decrementButton;
     private Button Reset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        decreaseButton.setOnClickListener(new View.OnClickListener() {
+        decrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (Integer.parseInt(textView.getText().toString() )!=0)
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     {
         textView=findViewById(R.id.TextView);
         incrementButton=findViewById(R.id.increment);
-        decreaseButton=findViewById(R.id.decrease);
+        decrementButton=findViewById(R.id.decrement);
         Reset=findViewById(R.id.reset);
     }
 
